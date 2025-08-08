a48f39d0efa65b5d416509695846dee40df34d59
manish <msrajpoot1000@gmail.com>
2025-08-08 13:03:30 +0530

    commit

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -100,7 +100,7 @@
 
                                  <div class="text-center mt-2">
                                      <h5>Welcome Back !</h5>
-                                     <p class="text-muted">Sign in to continue to webadmin.</p>
+                                     <p class="text-muted">Sign in to continue to Fitex Uniforms</p>
                                  </div>
 
                                  <div class="p-2 mt-4">
@@ -129,7 +129,7 @@
                                                          password?</a>
                                                  @endif
                                              </div>
-                                        
+
                                              <label class="form-label" for="password">Password</label>
                                              <div class="position-relative auth-pass-inputgroup input-custom-icon">
                                                  <span class="bx bx-lock-alt"></span>
@@ -140,7 +140,8 @@
                                                  <button type="button"
                                                      class="btn btn-link position-absolute h-100 end-0 top-0"
                                                      id="password-addon">
-                                                     <i class="mdi mdi-eye-outline font-size-18 text-muted" id="toggle-password-icon"></i>
+                                                     <i class="mdi mdi-eye-outline font-size-18 text-muted"
+                                                         id="toggle-password-icon"></i>
                                                  </button>
                                              </div>
                                              @error('password')
@@ -206,7 +207,7 @@
                  </div>
                  <!-- end row -->
 
-                 <div class="row">
+                 {{-- <div class="row">
                      <div class="col-lg-12">
                          <div class="text-center p-4">
                              <p>
@@ -219,7 +220,7 @@
                              </p>
                          </div>
                      </div>
-                 </div>
+                 </div> --}}
              </div>
          </div>
          <!-- end container -->
